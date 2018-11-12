446db884ce00bd6004b4f9775094674e534c51fe
근원 이 <Highfence@naver.com>
2018-11-12 11:53:01 +0900

    Complete FileIOHelper::GetFileBuffer. Test needed

--- SimpEngineLib/FileIOHelper.cpp
+++ SimpEngineLib/FileIOHelper.cpp
@@ -3,20 +3,23 @@
 
 namespace SimpEngine
 {
+	// Todo : 잘 작동하나 테스트 해보자. 너무 오랜만에 wchar_t를 썼다. 메모리 해제가 잘 되는지?
 	std::wstring FileIOHelper::GetFileBuffer(std::wstring& filePath)
 	{
-		wchar_t* buffer;
 		std::wifstream fileStream(filePath);
 
 		if (fileStream.is_open() == false)
-			return buffer;
+			return std::wstring(TEXT(""));
 
 		fileStream.seekg(0, std::ios::end);
 		auto fileLength = fileStream.tellg();
-		buffer = new wchar_t(fileLength);
+		wchar_t* buffer = new wchar_t(fileLength);
 		fileStream.seekg(0);
 		fileStream.read(buffer, fileLength);
+		
+		std::wstring stringBuffer = std::wstring(buffer);
+		delete[] buffer;
 
-		return std::wstring(buffer);
+		return stringBuffer;
 	}
 }
