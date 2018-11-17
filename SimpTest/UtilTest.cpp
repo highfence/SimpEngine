1e3097cb1106f9cf7ff439a7a7baf29771832815
Lee Kwenwon <highfence@naver.com>
2018-11-17 23:27:50 +0900

    Add Unit Test

---
+++ SimpTest/UtilTest.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SimpTest
+{		
+	TEST_CLASS(SimpFileIOTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: 테스트 코드를 여기에 입력합니다.
+		}
+
+	};
+}
